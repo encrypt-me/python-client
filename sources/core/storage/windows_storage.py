bfda3dce006189f8071abc69324f9b1473d49fbc
mixailkor <60221769+mixailkor@users.noreply.github.com>
2023-08-27 13:03:41 +0200

    changed path to local

--- sources/core/storage/windows_storage.py
+++ sources/core/storage/windows_storage.py
@@ -4,6 +4,5 @@ from sources.core.storage.storage import Storage
 
 class WindowsStorage(Storage):
     def __int__(self):
-        self.base_folder = os.path.join(os.getenv('APPDATA'), 'EncryptMe')
-        if not os.path.isdir(self.base_folder):
-            os.mkdir(self.base_folder)
+        self.base_folder = os.path.join(os.getenv('LOCALAPPDATA'), "EncryptMe")
+        os.mkdir(self.base_folder)
